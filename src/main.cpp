23930b024722498c10dc48c6bd8657894685085e
Austen Bruhn <asbru17@gmail.com>
2021-01-12 03:30:34 +0000

    TODO1: Add x,y coordinate start/stop

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,19 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    // Starting Coordinates
+    std::cout << "Enter the starting coordinates:" << "\n";
+    std::cout << "x: ";
+    std::cin >> start_x;
+    std::cout << "y: ";
+    std::cin >> start_y;
+    // Ending Coordinates
+    std::cout << "Enter the Ending coordinates:" << "\n";
+    std::cout << "x: ";
+    std::cin >> end_x;
+    std::cout << "y: ";
+    std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
